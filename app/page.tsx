86a7e1529600e6eeb91fdb74ee9245a7657f36d7
Michal Sakowski <michal.sakowski@jagex.com>
2025-06-25 08:46:19 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docss
           </a>
         </div>
       </main>
