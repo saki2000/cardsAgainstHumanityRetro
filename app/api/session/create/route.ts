1094f77353aacb3c71eded37a2e69e1d095b8caa
Michal Sakowski <sakijudo@gmail.com>
2025-07-06 22:41:04 +0100

    removed log i dont want

--- app/api/session/create/route.ts
+++ app/api/session/create/route.ts
@@ -1,20 +1,12 @@
 import axios from "axios";
 
 export async function POST(request: Request) {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  console.log("NEXT_PUBLIC_API_URL:", apiUrl); // Debugging line to check the API_URL
-
-  if (!apiUrl) {
-    // This will give you a clear error in your server logs if the variable is missing.
-    console.error(
-      "FATAL: NEXT_PUBLIC_API_URL environment variable is not set.",
-    );
-    throw new Error("Server is not configured correctly.");
-  } //TODO: Remove later if not needed
-
   try {
     const body = await request.json();
-    const response = await axios.post(`${apiUrl}/session/create`, body);
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/session/create`,
+      body,
+    );
     return new Response(JSON.stringify(response.data), { status: 200 });
   } catch (err) {
     console.error("Error:", err);
