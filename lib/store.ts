6b7e2693ed6bb35ae3cae61ada7aaf51b224cef0
Michal Sakowski <sakijudo@gmail.com>
2025-07-07 08:11:46 +0100

    added zustand package

---
+++ lib/store.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { create } from "zustand";
+
+interface Player {
+  id: string;
+  name: string;
+  email: string; //TODO: UNSURE IF NEEDED
+}
