5a7e510723df464faa09450826b01519e277be9d
Michal Sakowski <sakijudo@gmail.com>
2025-07-08 09:27:43 +0100

    added game room component and player list

---
+++ app/components/GamePage/GameRoom/GameRoom.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+import PlayerList from "../PlayerList/PlayerList";
+import { useUserStore } from "@/lib/userStore";
+import { useSession } from "next-auth/react";
+
+interface Props {
+  sessionCode: string;
+}
+
+export default function GameRoom({ sessionCode }: Props) {
+  const connectSocket = useUserStore((state) => state.connectSocket);
+  const joinSession = useUserStore((state) => state.joinSession);
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    connectSocket();
+
+    if (session?.user?.name && session.user?.email) {
+      joinSession({
+        username: session.user.name,
+        email: session.user.email,
+        sessionCode: sessionCode,
+      });
+    }
+  }, [session, sessionCode, connectSocket, joinSession]);
+
+  return (
+    <div className="h-screen border-4 border-white m-2">
+      <PlayerList />
+    </div>
+  );
+}
---
+++ app/components/GamePage/PlayerList/PlayerList.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useUserStore } from "@/lib/userStore";
+
+export default function PlayerList() {
+  const players = useUserStore((state) => state.players);
+
+  return (
+    <aside className="w-48 bg-white/50 p-4 rounded-lg shadow-md m-2">
+      <h2 className="text-xl font-bold mb-4">Players - {players.length}</h2>
+      <ul>
+        {players.map((player) => (
+          <li key={player.email} className="flex items-center gap-2 mb-2">
+            <span className="w-3 h-3 bg-green-400 rounded-full"></span>
+            <span>{player.username}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
