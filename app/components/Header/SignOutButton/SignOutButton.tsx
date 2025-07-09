1a9f98c4f37fdc7112ad80ed5395af3e676a6a10
Michal Sakowski <sakijudo@gmail.com>
2025-07-09 11:05:07 +0100

    refactored confirmation modal

--- app/components/Header/SignOutButton/SignOutButton.tsx
+++ app/components/Header/SignOutButton/SignOutButton.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import {
-  Dialog,
-  DialogPanel,
-  DialogTitle,
-  Transition,
-  TransitionChild,
-} from "@headlessui/react";
 import { Fragment, useState } from "react";
 import { signOut } from "next-auth/react";
 import { getLogOutUrl } from "@/lib/clientUtils";
+import ConfirmationModal from "../../ConfirmationModal/ConfirmationModal";
 
 export default function SignOutButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -37,66 +31,16 @@ export default function SignOutButton() {
           Sign out
         </button>
       </div>
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-50" onClose={closeModal}>
-          <TransitionChild
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black/30" />
-          </TransitionChild>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <TransitionChild
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <DialogPanel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-gray-800 p-6 text-left align-middle shadow-xl transition-all">
-                  <DialogTitle
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-white"
-                  >
-                    Confirm Sign Out
-                  </DialogTitle>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-300">
-                      Are you sure you want to sign out?
-                    </p>
-                  </div>
-
-                  <div className="mt-4 flex justify-end gap-8">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white hover:bg-gray-500"
-                      onClick={closeModal}
-                    >
-                      Cancel
-                    </button>
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white hover:bg-red-500"
-                      onClick={handleSignOut}
-                    >
-                      Sign Out
-                    </button>
-                  </div>
-                </DialogPanel>
-              </TransitionChild>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
+      <ConfirmationModal
+        isOpen={isOpen}
+        closeModal={() => {}}
+        onConfirm={handleSignOut}
+        onCancel={closeModal}
+        title="Confirm Sign Out"
+        message="Are you sure you want to sign out?"
+        okText="Sign Out"
+        cancelText="Cancel"
+      />
     </>
   );
 }
