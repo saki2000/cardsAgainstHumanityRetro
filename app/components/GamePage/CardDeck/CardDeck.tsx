e82a02679f92432289ee77212a99f90478e9ba85
Michal Sakowski <sakijudo@gmail.com>
2025-07-14 14:30:44 +0100

    fix types

--- app/components/GamePage/CardDeck/CardDeck.tsx
+++ app/components/GamePage/CardDeck/CardDeck.tsx
@@ -7,15 +7,16 @@ export default function CardDeck() {
   const hand = useCardDeckStore((state) => state.hand);
 
   return (
-    <div className="p-4 bg-gray-600 rounded-lg bottom-4 left-4 right-4 shadow-inner mb-14 mt-8">
-      <div className="flex gap-4 justify-center min-h-[14rem] items-center">
-        {hand.length > 0 ? (
-          hand.map((card) => (
-            <Card key={card.id} id={card.id} text={card.text} />
-          ))
-        ) : (
-          <p className="text-gray-500">No cards in hand.</p>
-        )}
+    <div className="p-4 bg-gray-600 rounded-lg bottom-4 left-4 right-4 shadow-inner mb-14 mt-8 max-w-full">
+      <div className="flex flex-wrap gap-2 justify-center items-center">
+        {hand.map((card) => (
+          <Card
+            key={card.id}
+            id={card.id.toString()}
+            text={card.content}
+            isDraggable={true}
+          />
+        ))}
       </div>
     </div>
   );
--- app/components/GamePage/GameContainer/GameContainer.tsx
+++ app/components/GamePage/GameContainer/GameContainer.tsx
@@ -3,11 +3,16 @@ import { DndContext, DndContextProps } from "@dnd-kit/core";
 import TableSlot from "../TableSlot/TableSlot";
 
 import { useCardDeckStore } from "@/lib/CardDeckStore";
-import { useGameStore } from "@/lib/gameStore";
+import { useGameStore } from "@/lib/GameStore";
 import Card from "../Card/Card";
 
-export default function GameContainer() {
-  const { slots, playCard } = useCardDeckStore();
+interface Props {
+  sessionCode: string;
+}
+
+export default function GameContainer({ sessionCode }: Props) {
+  const { slots } = useCardDeckStore();
+  const playCard = useGameStore((state) => state.playCard);
   const isCurrentUserCardHolder = useGameStore(
     (state) => state.isCurrentUserCardHolder,
   );
@@ -15,10 +20,10 @@ export default function GameContainer() {
   const handleDragEnd: DndContextProps["onDragEnd"] = (event) => {
     const { active, over } = event;
     if (over) {
-      const cardId = active.id as string;
+      const cardId = active.id;
       const slotId = over.id as "slot1" | "slot2" | "slot3";
 
-      playCard(cardId, slotId);
+      playCard(sessionCode, Number(cardId), slotId);
     }
   };
 
@@ -26,23 +31,35 @@ export default function GameContainer() {
     <DndContext onDragEnd={handleDragEnd}>
       <div className="flex flex-1 flex-col items-center justify-center p-8 space-y-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
-          <TableSlot id="slot1">
+          <TableSlot id="slot1" isOccupied={!!slots.slot1}>
             {slots.slot1 ? (
-              <Card id={slots.slot1.id} text={slots.slot1.text} />
+              <Card
+                id={slots.slot1?.id ? String(slots.slot1.id) : ""}
+                text={slots.slot1?.content ?? ""}
+                isDraggable={false}
+              />
             ) : (
               <span className="text-gray-400">Card Slot</span>
             )}
           </TableSlot>
-          <TableSlot id="slot2">
+          <TableSlot id="slot2" isOccupied={!!slots.slot2}>
             {slots.slot2 ? (
-              <Card id={slots.slot2.id} text={slots.slot2.text} />
+              <Card
+                id={slots.slot2?.id ? String(slots.slot2.id) : ""}
+                text={slots.slot2?.content ?? ""}
+                isDraggable={false}
+              />
             ) : (
               <span className="text-gray-400">Card Slot</span>
             )}
           </TableSlot>
-          <TableSlot id="slot3">
+          <TableSlot id="slot3" isOccupied={!!slots.slot3}>
             {slots.slot3 ? (
-              <Card id={slots.slot3.id} text={slots.slot3.text} />
+              <Card
+                id={slots.slot3?.id ? String(slots.slot3.id) : ""}
+                text={slots.slot3?.content ?? ""}
+                isDraggable={false}
+              />
             ) : (
               <span className="text-gray-400">Card Slot</span>
             )}
