360cf496005849acd7546df5b337add9a811b46d
Michal Sakowski <sakijudo@gmail.com>
2025-07-14 14:28:34 +0100

    fixes on card to be not dragable and text size

--- app/components/GamePage/Card/Card.tsx
+++ app/components/GamePage/Card/Card.tsx
@@ -5,12 +5,14 @@ import { useDraggable } from "@dnd-kit/core";
 interface Props {
   id: string;
   text: string;
+  isDraggable?: boolean;
 }
 
-export default function Card({ id, text }: Props) {
+export default function Card({ id, text, isDraggable }: Props) {
   const { attributes, listeners, setNodeRef, transform, isDragging } =
     useDraggable({
       id: id,
+      disabled: !isDraggable,
     });
 
   const cornerText = "R";
@@ -23,13 +25,15 @@ export default function Card({ id, text }: Props) {
       }
     : undefined;
 
+  const cursorClass = isDraggable ? "cursor-grab" : "cursor-default";
+
   return (
     <div
       ref={setNodeRef}
       style={style}
-      {...listeners}
+      {...(isDraggable ? listeners : {})}
       {...attributes}
-      className="relative p-4 h-48 w-40 bg-black text-white border-2 border-gray-600 rounded-lg shadow-lg cursor-grab flex items-center justify-center"
+      className={`relative p-4 h-48 w-40 bg-black text-white border-2 border-gray-600 rounded-lg shadow-lg flex items-center justify-center ${cursorClass}`}
     >
       <div className="absolute top-2 left-2 h-4 w-4 bg-white text-black rounded-sm flex items-center justify-center p-1">
         <span className="text-small font-bold leading-tight text-center">
@@ -37,7 +41,7 @@ export default function Card({ id, text }: Props) {
         </span>
       </div>
 
-      <p className="text-center font-bold px-6">{text}</p>
+      <p className="text-center font-bold">{text}</p>
 
       <div className="absolute bottom-2 right-2 h-4 w-4 bg-white text-black rounded-sm flex items-center justify-center p-1 transform rotate-180">
         <span className="text-small font-bold leading-tight text-center">
