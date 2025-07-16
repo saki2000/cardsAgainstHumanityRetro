0057e8604c48543cc30fbf7f2e826b85a83c1c6a
Michal Sakowski <sakijudo@gmail.com>
2025-07-16 11:18:56 +0100

    made styling better

--- app/components/AnswerDeck/AnswerDeck.tsx
+++ app/components/AnswerDeck/AnswerDeck.tsx
@@ -2,7 +2,7 @@ import AnswerCard from "../GamePage/AnswerCard/AnswerCard";
 
 export default function AnswerDeck() {
   return (
-    <div className="p-4 bg-gray-600 rounded-lg bottom-4 left-4 right-4 shadow-inner mb-14 mt-8 max-w-full">
+    <div className="py-6 px-32 bg-gray-600 rounded-lg bottom-4 left-4 right-4 shadow-inner mb-14 mt-8 max-w-full">
       <AnswerCard />
     </div>
   );
